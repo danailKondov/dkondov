5459c786ea68ff54a7e9a4cbe4f7273abffe1b69
Danail <dkondov@yandex.ru>
2017-07-21 01:59:15 +0400

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -12,6 +12,6 @@ public class Calculate {
 	* @param args параметры запуска программы
 	*/
 	public static void main (String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello World");
 	}
 }
