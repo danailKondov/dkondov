d3908b749848dc78b279b41e647ba0e2c7d6e6d9
Danail <dkondov@yandex.ru>
2017-07-21 11:48:51 +0400

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,17 +1,17 @@
 package ru.job4j;
 
 /**
-* Class Класс для осуществления арифметических операций
-* @autor dkondov
+* Class Класс для осуществления арифметических операций.
 * @since 21/07/2017
 * @version 1
 */
 public class Calculate {
-	
-	/** Основной метод main
+
+	/**
+	* Основной метод main.
 	* @param args параметры запуска программы
 	*/
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 }
