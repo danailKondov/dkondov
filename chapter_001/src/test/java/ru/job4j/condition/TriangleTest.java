af9e377153b9ef2b762eb4507990c5e92bae9b72
Danail <dkondov@yandex.ru>
2017-07-25 14:04:15 +0400

    новый TriangleTest

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -38,8 +38,8 @@ public class TriangleTest {
 	public void whenAllPointsOnLineException() {
 		// создаем три объекта класса Point.
 		Point a = new Point(0, 0);
-		Point b = new Point(0, 0);
-		Point c = new Point(0, 0);
+		Point b = new Point(1, 1);
+		Point c = new Point(2, 2);
 		// Создаем объект треугольник и передаем в него объекты точек.
 		Triangle triangle = new Triangle(a, b, c);
 		// Вычисляем площадь.
