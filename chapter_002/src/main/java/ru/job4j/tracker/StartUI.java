62b12ccceee6f53ee3070e8efe25e267f6aae2ca
Danail <dkondov@yandex.ru>
2017-07-31 17:02:20 +0400

    1. Используя класс ConsoleInput в классе StartUI... [#20538] (исправленный)

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -19,6 +19,11 @@ public class StartUI {
 	private Tracker tracker;
 
 	/**
+	* Exit constant.
+	**/
+	private static final String EXIT = "6";
+
+	/**
 	* Constructor with parameters.
 	**/
 	public StartUI(Input input, Tracker tracker) {
@@ -51,45 +56,87 @@ public class StartUI {
 			"Select:"
 			);
 			String answer = input.ask("");
-			if (answer.equals("6")) {
+			if (answer.equals(EXIT)) {
 				break;
 			} else if (answer.equals("0")) {
-				String name = input.ask("Enter user name: ");
-				String description = input.ask("Enter description: ");
-				//String cr = input.ask("Enter time of creation: ");
-				//long created = 0L;
-				//try {
-				//	created = Long.parseLong(cr); // try-catch
-				//} catch (NumberFormatException e) {
-				//	e.printStackTrace();
-				//}
-				tracker.add(new Item(name, description, 130L));
+				addNewItem();
 			} else if (answer.equals("1")) {
-				for (Item item : tracker.findAll()) {
-					System.out.println(item);
-				}
+				showAllItems();
 			} else if (answer.equals("2")) {
-				String id = input.ask("Enter id of edited item: ");
-				String name = input.ask("Enter new name: ");
-				String desc = input.ask("Enter new description: ");
-				long created = tracker.findByID(id).getCreated();
-				Item newItem = new Item(name, desc, created);
-				newItem.setID(id);
-				tracker.update(newItem);
+				editItem();
 			} else if (answer.equals("3")) {
-				String id = input.ask("Enter id: ");
-				tracker.delete(tracker.findByID(id));
+				deleteItem();
 			} else if (answer.equals("4")) {
-				String id = input.ask("Enter id: ");
-				System.out.println(tracker.findByID(id));
+				findItemByID();
 			} else if (answer.equals("5")) {
-				String name = input.ask("Enter name: ");
-				for (Item item : tracker.findByName(name)) {
-					System.out.println(item);
-				}
+				findItemsByName();
 			} else {
 				System.out.println("Wrong command, enter new one: ");
 			}
+		}
+	}
+
+	/**
+	* Add new item option functionality.
+	**/
+	private void addNewItem() {
+		String name = input.ask("Enter user name: ");
+		String description = input.ask("Enter description: ");
+		//String cr = input.ask("Enter time of creation: ");
+		//long created = 0L;
+		//try {
+		//	created = Long.parseLong(cr); // try-catch
+		//} catch (NumberFormatException e) {
+		//	e.printStackTrace();
+		//}
+		tracker.add(new Item(name, description, 130L));
+	}
+
+	/**
+	* Show all items option functionality.
+	**/
+	private void showAllItems() {
+		for (Item item : tracker.findAll()) {
+			System.out.println(item);
+		}
+	}
+
+	/**
+	* Edit item option functionality.
+	**/
+	private void editItem() {
+		String id = input.ask("Enter id of edited item: ");
+		String name = input.ask("Enter new name: ");
+		String desc = input.ask("Enter new description: ");
+		long created = tracker.findByID(id).getCreated();
+		Item newItem = new Item(name, desc, created);
+		newItem.setID(id);
+		tracker.update(newItem);
+	}
+
+	/**
+	* Delete item option functionality.
+	**/
+	private void deleteItem() {
+		String id = input.ask("Enter id: ");
+		tracker.delete(tracker.findByID(id));
+	}
+
+	/**
+	* Find item by ID option functionality.
+	**/
+	private void findItemByID() {
+		String id = input.ask("Enter id: ");
+		System.out.println(tracker.findByID(id));
+	}
+
+	/**
+	* Find items by name option functionality.
+	**/
+	private void findItemsByName() {
+		String name = input.ask("Enter name: ");
+		for (Item item : tracker.findByName(name)) {
+			System.out.println(item);
 		}
 	}
 }
