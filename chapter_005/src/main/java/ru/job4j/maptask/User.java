199b289fcdaf5b6af23f69cc89dca04b7962b923
Danail <dkondov@yandex.ru>
2017-08-28 21:59:42 +0400

    5.5.2. Не перекрывать equals hashCode [#1005]

---
+++ chapter_005/src/main/java/ru/job4j/maptask/User.java
@@ -0,0 +1,69 @@
+package ru.job4j.maptask;
+
+//import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Class user.
+ *
+ * @since 28/08/2017
+ * @version 1
+ */
+public class User {
+
+    /**
+     * User's name.
+     */
+    private String name;
+
+    /**
+     * Number of user's children.
+     */
+    private int children;
+
+    /**
+     * User's birthday.
+     */
+    private Calendar birthday;
+
+    /**
+     * Constructor.
+     *
+     * @param name user's name
+     * @param children number of user's children
+     * @param birthYear - year of birth
+     * @param birthMonth - month of birth
+     * @param birthDayOfMonth - day of birth
+     */
+    public User(String name, int children, int birthYear, int birthMonth, int birthDayOfMonth) {
+        this.name = name;
+        this.children = children;
+        this.birthday = new GregorianCalendar(birthYear, birthMonth, birthDayOfMonth);
+    }
+
+//    /**
+//     * Default constructor.
+//     */
+//    public User() {
+//    }
+//
+//    @Test
+//    public void map() {   // не работает из-за наличия конструктора с параметрами
+//    }
+
+    public static void main(String[] args) {
+        User one = new User("Masha", 2, 1980, 9, 15);
+        User two = new User("Masha", 2, 1980, 9, 15);
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(one, new Object());
+        map.put(two, new Object());
+
+        System.out.println(map);
+    }
+}
